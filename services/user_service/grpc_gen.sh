372069971de38b2b7bb4e8a7b19a44a3847a0029
fahim-ash <ashhabfahim@gmail.com>
2024-06-30 22:11:59 +0600

    user_service: bash command updated

--- services/user_service/grpc_gen.sh
+++ services/user_service/grpc_gen.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
+PROTO_DIR="./proto"  # Directory where your .proto file is located
+OUT_DIR="./src/grpc"  # Directory where you want to output generated files
+
+# Generate gRPC code
+grpc_tools_node_protoc \
+  --js_out=import_style=commonjs,binary:${OUT_DIR} \
+  --grpc_out=${OUT_DIR} \
+  --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
+  ${PROTO_DIR}/user_service.proto
+
+# Generate TypeScript definitions
 grpc_tools_node_protoc \
   --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
-  --plugin=protoc-gen-grpc=./node_modules/.bin/grpc_tools_node_protoc_plugin \
-  --ts_out=src/grpc \
-  --grpc_out=src/grpc \
-  --proto_path=src/grpc \
-    src/grpc/user_service.proto
+  --ts_out=${OUT_DIR} \
+  ${PROTO_DIR}/user_service.proto
+
+echo "gRPC and TypeScript files generated successfully!"
